b224ae4f2c532aba793f6a3e468e7955ac508431
Kirill Hobyan <82779687+parumpupum@users.noreply.github.com>
2021-05-22 14:02:28 +0300

    Update PlayersSeparator.m

--- Objc_Task3/ios.stage-task/Exercises/Exercise-2/PlayersSeparator.m
+++ Objc_Task3/ios.stage-task/Exercises/Exercise-2/PlayersSeparator.m
@@ -3,7 +3,18 @@
 @implementation PlayersSeparator
 
 - (NSInteger)dividePlayersIntoTeams:(NSArray<NSNumber *>*)ratingArray {
-    return 0;
+
+    NSInteger count = 0, n = ratingArray.count;
+    for (int i = 0; i < n; i++)
+        for (int j = i + 1; j < n; j++)
+            for (int k = j + 1; k < n; k++)
+                if (ratingArray[i].intValue < ratingArray[j].intValue &&
+                    ratingArray[j].intValue < ratingArray[k].intValue) count++;
+                else if (ratingArray[i].intValue > ratingArray[j].intValue &&
+                         ratingArray[j].intValue > ratingArray[k].intValue) count++;
+    
+    return count;
+    
 }
 
 @end
