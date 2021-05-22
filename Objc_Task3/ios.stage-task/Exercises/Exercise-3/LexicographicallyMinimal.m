8196e59a8ca579b51c6cbb5b3cbf4434592927b2
Kirill Hobyan <82779687+parumpupum@users.noreply.github.com>
2021-05-22 15:24:13 +0300

    Update LexicographicallyMinimal.m

--- Objc_Task3/ios.stage-task/Exercises/Exercise-3/LexicographicallyMinimal.m
+++ Objc_Task3/ios.stage-task/Exercises/Exercise-3/LexicographicallyMinimal.m
@@ -9,7 +9,50 @@
 @implementation LexicographicallyMinimal
 
 -(NSString *)findLexicographicallyMinimalForString1:(NSString *)string1 andString2:(NSString *)string2 {
-    return nil;
+    
+    int i = 0, j = 0;
+    NSMutableString *result = [[NSMutableString alloc] init];
+    while (i < string1.length && j < string2.length){
+        if ([string1 characterAtIndex:i] < [string2 characterAtIndex:j]){
+            [result appendString:[NSString stringWithFormat: @"%c", [string1 characterAtIndex:i]]];
+            i++;
+        }
+        else if ([string1 characterAtIndex:i] > [string2 characterAtIndex:j]){
+            [result appendString:[NSString stringWithFormat: @"%c", [string2 characterAtIndex:j]]];
+            j++;
+        }
+        else{
+            int q = 1;
+            BOOL key = 0;
+            while (i + q < string1.length && j + q < string2.length){
+                if ([string1 characterAtIndex:i + q] < [string2 characterAtIndex:j + q]){
+                    [result appendString:[NSString stringWithFormat: @"%c", [string1 characterAtIndex:i]]];
+                    i++;
+                    key = 1;
+                    break;
+                }
+                else if ([string1 characterAtIndex:i + q] > [string2 characterAtIndex:j + q]){
+                    [result appendString:[NSString stringWithFormat: @"%c", [string2 characterAtIndex:j]]];
+                    j++;
+                    key = 1;
+                    break;
+                }
+                else q++;
+            }
+            if (key == 0){
+                [result appendString:[NSString stringWithFormat: @"%c", [string1 characterAtIndex:i]]];
+                i++;
+            }
+        }
+    }
+    
+    for (i; i < string1.length; i++)
+        [result appendString:[NSString stringWithFormat: @"%c", [string1 characterAtIndex:i]]];
+    
+    for (j; j < string2.length; j++)
+        [result appendString:[NSString stringWithFormat: @"%c", [string2 characterAtIndex:j]]];
+    
+    return result;
 }
 
 @end
